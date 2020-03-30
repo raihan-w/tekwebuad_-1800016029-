158f2eeb19d43fb4c897ee55365968845ad2aa24
Raihanw <raihan1800016029@wbmail.uad.ac.id>
2020-03-30 20:32:37 +0700

    tekweb1800016029

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+import { StudentComponent } from './pages/student/student.component';
+
+const routes: Routes = [
+	{
+		path:'home',
+		component:HomeComponent
+	},
+	{
+		path:"student",
+		component:StudentComponent
+	}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentComponent } from './pages/student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
